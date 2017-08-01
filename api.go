3ee80a97f8d54b7816be51d6be0cfc6c6ed0962e
Anthony Nixon <ajn0592@gmail.com>
2017-07-31 19:40:51 -0500

    update PUT method and add inverse color

--- api.go
+++ api.go
@@ -1,38 +1,39 @@
 package main
 
 import (
-	//"database/sql"
-	//_ "github.com/go-sql-driver/mysql"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
 
 	"fmt"
 	"net/http"
+	"os"
 	"strings"
-	//"os"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	//DB_USER := os.Getenv("CRONDBUSER")
-	//DB_PASS := os.Getenv("CRONDBPASS")
-	//DB_HOST := os.Getenv("DBHOST")
-	//
-	//dsn := DB_USER + ":" + DB_PASS + "@tcp(" + DB_HOST + ":3306)/cronager?parseTime=true"
-	//
-	//db, err := sql.Open("mysql", dsn)
-	//if err != nil {
-	//	fmt.Print(err.Error())
-	//}
-	//defer db.Close()
-	//// make sure our connection is available
-	//err = db.Ping()
-	//if err != nil {
-	//	fmt.Print(err.Error())
-	//}
+	DB_USER := os.Getenv("CRONDBUSER")
+	DB_PASS := os.Getenv("CRONDBPASS")
+	DB_HOST := os.Getenv("DBHOST")
+
+	dsn := DB_USER + ":" + DB_PASS + "@tcp(" + DB_HOST + ":3306)/cronager?parseTime=true"
+
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+	defer db.Close()
+	// make sure our connection is available
+	err = db.Ping()
+	if err != nil {
+		fmt.Print(err.Error())
+	}
 	type Turnthiswhite struct {
-		Number int    `json:"number"`
-		Color  string `json:"color"`
+		Number       int    `json:"number"`
+		Color        string `json:"color"`
+		InverseColor string `json:"inverseColor"`
 	}
 
 	currentNumber := 0
@@ -44,20 +45,28 @@ func main() {
 	router.GET("/color", func(c *gin.Context) {
 		var turnthiswhite Turnthiswhite
 
-		currentNumber++
 		turnthiswhite.Number = currentNumber
-		turnthiswhite.Color = strings.Replace(fmt.Sprintf("#%-6x", currentNumber), " ", "0", -1)
+		turnthiswhite.Color = strings.Replace(fmt.Sprintf("#%6x", currentNumber), " ", "0", -1)
+		turnthiswhite.InverseColor = strings.Replace(fmt.Sprintf("#%6x", (16777215-currentNumber)), " ", "0", -1)
 
+		c.Header("Access-Control-Allow-Origin", "*")
 		c.JSON(http.StatusOK, turnthiswhite)
 	})
 
 	router.PUT("/color", func(c *gin.Context) {
+		var turnthiswhite Turnthiswhite
+		currentNumber++
+		turnthiswhite.Number = currentNumber
+		turnthiswhite.Color = strings.Replace(fmt.Sprintf("#%6x", currentNumber), " ", "0", -1)
+		turnthiswhite.InverseColor = strings.Replace(fmt.Sprintf("#%6x", (16777215-currentNumber)), " ", "0", -1)
 
-		c.JSON(http.StatusOK, gin.H{})
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.JSON(http.StatusOK, turnthiswhite)
 	})
 
 	router.OPTIONS("/color", func(c *gin.Context) {
 		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Methods", "GET,PUT")
 		c.Header("Access-Control-Allow-Headers", "access-control-allow-origin, access-control-allow-headers")
 		c.JSON(http.StatusOK, struct{}{})
 	})
